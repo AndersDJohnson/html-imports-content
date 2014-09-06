de3759796e39dcab5776a0a356c7652f9b823177
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-09-06 15:23:40 -0500

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# html-imports-content
+
+HTML Imports with content inlining, or replacing by selector.
+
+Features
+* Declarative definition to encourage future search engine support.
+* Improve speed of web by allowing site boilerplate HTML to be cached by client browsers independently of page-specific content.
+
+Browser support:
+* [Can I use](http://caniuse.com/imports)
+
+Specs:
+* [W3C](http://www.w3.org/TR/2013/WD-html-imports-20130514/)
+
+Polyfills:
+* [Polymer](https://github.com/polymer/HTMLImports)
+
---
+++ html-imports-content.js
@@ -0,0 +1,76 @@
+(function (out) {
+
+
+  var forEach = Array.prototype.forEach;
+
+  var insertNodesBefore = function (nodes, afterNode) {
+    var node = nodes[nodes.length - 1];
+    while (node) {
+      var cloned = node.cloneNode(true);
+      afterNode.parentNode.insertBefore(cloned, afterNode);
+      node = node.previousSibling;
+      afterNode = cloned;
+    }
+  };
+
+  var removeNode = function (node) {
+    node.parentNode.removeChild(node);
+  };
+
+
+  var importContent = function (options, context) {
+
+    options = options || {};
+    context = context || this;
+
+    var imported = context.import;
+    var importedBody = imported.body;
+    var importedBodyNodes = importedBody.childNodes;
+
+    if (context.getAttribute('inline') === '') {
+
+      insertNodesBefore(importedBodyNodes, context);
+      removeNode(context);
+
+    }
+    else if (context.getAttribute('replace')) {
+
+      var replaceAttr = context.getAttribute('replace');
+      var nodes = document.querySelectorAll(replaceAttr);
+
+      forEach.call(nodes, function (node) {
+        if (typeof options.timeout === 'number') {
+          setTimeout(function () {
+            insertNodesBefore(importedBodyNodes, node);
+            removeNode(node);
+            removeNode(context);
+          }, options.timeout);
+        }
+        else {
+          insertNodesBefore(importedBodyNodes, node);
+          removeNode(node);
+          removeNode(context);
+        }
+      });
+
+    }
+
+  };
+
+  var importsContent = function (options) {
+
+    options = options || {};
+    options.timeout = options.timeout || null;
+
+    var imports = document.querySelectorAll('link[rel="import"]');
+
+    forEach.call(imports, function (importer) {
+      importContent.call(importer, options);
+    });
+  };
+
+  out.importContent = importContent;
+  out.importsContent = importsContent;
+
+  return out;
+})(this);
